59391816b905ca1242a9b3a0a4b9d7a707faf721
chemmy-jack <kuojack0114@gmail.com>
2025-10-14 22:06:24 +0800

    change some names and prepare to add RS485 part, but currently it gives sementation fault

--- kilin_fpga_driver/include/fpga_handler.hpp
+++ kilin_fpga_driver/include/fpga_handler.hpp
@@ -127,3 +127,49 @@ public:
 };
 
 #endif
+
+// for RS485 protocol
+/*	Packet RX Information
+
+	Byte		|   0~4  |    5    |    6    |  7~10   |   11    |   12    |  13~16  |   17    |   18    |
+	Function	| Header |   CMD1  | SUBCMD1 |  Data1  |   CMD2  | SUBCMD2 |  Data2  |Checksum1|Checksum2|
+
+	Byte0~4		: Header, 0xFF*5
+	------------------------------------------------------------------
+	Byte5		: CMD1, commnad for steering motor
+	Byte6		: SUBCMD1, sub commnad for steering motor
+	Byte7~10	: Data1, data for steering motor
+	------------------------------------------------------------------
+	Byte11		: CMD2, commnad for wheel hub motor
+	Byte12		: SUBCMD2, sub commnad for wheel hub motor
+	Byte12~16	: Data2, data for wheel hub motor
+	------------------------------------------------------------------
+	Byte17		: Checksum1(CKS1), calculate by following steps
+				  Step1 : XOR(^) all packet data, exclusive Header, CKS1&2
+				  Step2 : Make result of Step1 & 0xFE
+	Byte18		: Checksum2(CKS1), (~CKS1) & 0xFE
+*/
+
+
+/*	Packet TX Information
+
+	Byte		|   0~4  |    5    |   6~9   |    10   |  11~14  |    15   |   16~19 |    20   |  21~24  |   25    |   26    |
+	Function	| Header |   CMD1  |   POS1  |  STAT1  |    I1   |   CMD2  |   POS2  |  STAT2  |    I2   |Checksum1|Checksum2|
+
+	Byte0~4		: Header, 0xFF*5
+	------------------------------------------------------------------
+	Byte5		: CMD1, RX CMD/Version for steering motor, firmware version(bit4~6) + CMD(0~3)
+	Byte6~9		: POS1, position for steering motor
+	Byte10		: STAT1, status for steering motor, hall-status(bit4~5) + system state(0~3)
+	Byte11~14	: I1, current of steering motor
+	------------------------------------------------------------------
+	Byte15		: CMD2, RX CMD/Version for wheel hub motor, firmware version(bit4~6) + CMD(0~3)
+	Byte16~19	: POS2, position for wheel hub motor
+	Byte20		: STAT2, status for wheel hub motor
+	Byte21~24	: I2, current of wheel hub motor
+	------------------------------------------------------------------
+	Byte25		: Checksum1(CKS1), calculate by following steps
+				  Step1 : XOR(^) all packet data, exclusive Header, CKS1&2
+				  Step2 : Make result of Step1 & 0xFE
+	Byte26		: Checksum2(CKS1), (~CKS1) & 0xFE
+*/
